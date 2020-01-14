d4b11efa4be4561ed9bca186a53723e9be525f41
Hanna Lysenko <hannatkach92@gmail.com>
2020-01-13 23:56:16 -0600

    PASV User Register + Login

--- test/specs/register.js
+++ test/specs/register.js
@@ -1,7 +1,7 @@
 const {expect} = require('chai');
 const email = Math.random()+'@gmail.com';
 const firstName = "John";
-const password = 'dgdfdvsdgdrhrdzg';
+const password = 'dgdfdvsdgd2597dzg';
 
 describe('Register page', () => {
     before(() => {
@@ -35,54 +35,64 @@ describe('Register page', () => {
     it('should fill up register first name field', () => {
         const element = $('form input[name="firstName"]');
         element.setValue(firstName);
-        browser.pause(2000);
     });
 
     it('should fill up register last name field', () => {
         const element = $('form input[name="lastName"]');
         element.setValue('Smith');
-        browser.pause(2000);
     });
 
     it('should fill up phone number field', () => {
         const element = $('form input[name="phone"]');
         element.setValue('19165975597');
-        browser.pause(2000);
     });
 
     it('should fill up email field', () => {
         const element = $('form input[name="email"]');
         element.setValue(email);
-        browser.pause(2000);
     });
 
     it('should fill up password field', () => {
         const element = $('form input[name="password"]');
         element.setValue(password);
-        browser.pause(2000);
     });
 
     it('should fill up about field', () => {
         const element = $('form textarea[name="about"]');
         element.setValue('good mood');
-        browser.pause(2000);
     });
 
     it('should fill up goals field', () => {
         const element = $('form textarea[name="goals"]');
-        element.setValue('eat sleep');
-        browser.pause(2000);
+        element.setValue('make life better');
     });
 
     it('should choose English level dropdown', () => {
         const element = $('form select[name="englishLevel"]');
         element.selectByVisibleText('Native');
-        browser.pause(2000);
     });
 
     it('should click a button Submit', () => {
         const element = $('form button[type="submit"]');
         element.click();
-        browser.pause(2000);
+        browser.pause(1000);
     });
-})
+});
+
+describe('login page', () =>{
+    it('should fill up email field', () => {
+        const element = $('form input[name="email"]');
+        element.setValue(email);
+    });
+
+    it('should fill up password field', () => {
+        const element = $('form input[name="password"]');
+        element.setValue(password);
+    });
+
+    it('should click a button Login', () => {
+        const element = $('form button[type="submit"]');
+        element.click();
+        browser.pause(5000);
+    });
+});
