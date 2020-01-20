425c791cd638466c635e705ec43beac9c2d09706
Hanna Lysenko <hannatkach92@gmail.com>
2020-01-20 16:22:43 -0600

    PASV User Register + Login + New User Page (done)

--- test/specs/register.spec.js
+++ test/specs/register.spec.js
@@ -1,43 +1,6 @@
 const {expect} = require('chai');
 
-const { URL_REGISTER, user, pageRegister, pageRegisterSelectors, pageLogin, pageConfirmation } = require('./register_data');
-
-const user = {
-    firstName: "John",
-    lastName: 'Smith',
-    password: 'dgdfdvsdgd2597dzg',
-    phone: '19165975597',
-    email: Math.random() + '@gmail.com',
-    about: 'good mood',
-    goals: 'make life better',
-    englishLevel: 'Native'
-};
-
-const page = {
-    title: 'Progress Monitor',
-    h1: 'User Register',
-    description: 'Profiles with fictitious or dummy data will be deleted.',
-    buttonText: 'Submit'
-
-};
-
-const pageRegisterSelectors = {
-    h1: h1,
-    description: 'p',
-    submitButton: 'form button',
-    firstNameInput: 'form input[name="firstName"]',
-    lastNameInput: 'form input[name="lastName"]',
-    phoneInput: 'form input[name="phone"]',
-    emailInput: 'form input[name="email"]',
-    passwordInput: 'form input[name="password"]',
-    aboutInput: 'form textarea[name="about"]',
-    goalsInput: 'form textarea[name="goals"]',
-    englishLevelInput: 'form select[name="englishLevel"]'
-};
-
-const page_login = {
-    h1: 'You are a new user'
-};
+const {URL_REGISTER, user, page, pageRegisterSelectors, pageLogin } = require('./register_data');
 
 describe('Register page', () => {
     it('should have a correct title', () => {
@@ -48,9 +11,9 @@ describe('Register page', () => {
     });
 
     it('should have a correct title', () => {
-        const actualH1text = $(pageRegisterSelectors.h1).getText();
-        const expectedH1Text = page.h1;
-        expect(actualH1text).equal(expectedH1Text);
+        const actual = $(pageRegisterSelectors.h1).getText();
+        const expected = page.h1;
+        expect(actual).equal(expected);
     });
 
     it('should have a correct description', () => {
@@ -71,42 +34,42 @@ describe('Register page', () => {
     });
 
     it('should fill up register last name field', () => {
-        const element = $('form input[name="lastName"]');
+        const element = $(pageRegisterSelectors.lastNameInput);
         element.setValue(user.lastName);
     });
 
     it('should fill up phone number field', () => {
-        const element = $('form input[name="phone"]');
+        const element = $(pageRegisterSelectors.phoneInput);
         element.setValue(user.phone);
     });
 
     it('should fill up email field', () => {
-        const element = $('form input[name="email"]');
+        const element = $(pageRegisterSelectors.emailInput);
         element.setValue(user.email);
     });
 
     it('should fill up password field', () => {
-        const element = $('form input[name="password"]');
+        const element = $(pageRegisterSelectors.passwordInput);
         element.setValue(user.password);
     });
 
     it('should fill up about field', () => {
-        const element = $('form textarea[name="about"]');
+        const element = $(pageRegisterSelectors.aboutInput);
         element.setValue(user.about);
     });
 
     it('should fill up goals field', () => {
-        const element = $('form textarea[name="goals"]');
+        const element = $(pageRegisterSelectors.goalsInput);
         element.setValue(user.goals);
     });
 
     it('should choose English level dropdown', () => {
-        const element = $('form select[name="englishLevel"]');
+        const element = $(pageRegisterSelectors.englishLevelInput);
         element.selectByVisibleText(user.englishLevel);
     });
 
     it('should click a button Submit', () => {
-        const element = $('form button[type="submit"]');
+        const element = $(pageRegisterSelectors.submitButton);
         element.click();
         browser.pause(5000);
     });
@@ -114,17 +77,17 @@ describe('Register page', () => {
 
 describe('Login Page', () =>{
     it('should fill up email field', () => {
-        const element = $('form input[name="email"]');
+        const element = $(pageRegisterSelectors.emailInput);
         element.setValue(user.email);
     });
 
     it('should fill up password field', () => {
-        const element = $('form input[name="password"]');
+        const element = $(pageRegisterSelectors.passwordInput);
         element.setValue(user.password);
     });
 
     it('should click a button Login', () => {
-        const element = $('form button[type="submit"]');
+        const element = $(pageRegisterSelectors.submitButton);
         element.click();
         browser.pause(5000);
     });
@@ -132,8 +95,8 @@ describe('Login Page', () =>{
 
 describe('Confirmation Register Page', () => {
     it('should have the correct title', () => {
-        const actual = $('h1').getText();
-        const expected = page_login.h1;
+        const actual = $(pageRegisterSelectors.h1).getText();
+        const expected = pageLogin.h1;
         expect(actual).equal(expected);
     });
 });
--- test/specs/register_data.js
+++ test/specs/register_data.js
@@ -1,2 +1,42 @@
-const URL_REGISTER = 'https://stage.pasv.us/user/register';
-module.exports = { URL_REGISTER, user, pageRegister, pageRegisterSelectors, pageLogin, pageConfirmation };
+const HOST = 'https://stage.pasv.us';
+const URL_REGISTER = `${HOST}/user/register`;
+const URL_LOGIN = `${HOST}/user/login`;
+
+const user = {
+    firstName: "John",
+    lastName: 'Smith',
+    password: 'dgdfdvsdgd2597dzg',
+    phone: '19165975597',
+    email: Math.random() + '@gmail.com',
+    about: 'good mood',
+    goals: 'make life better',
+    englishLevel: 'Native'
+};
+
+const page = {
+    title: 'Progress Monitor',
+    h1: 'User Register',
+    description: 'Profiles with fictitious or dummy data will be deleted.',
+    buttonText: 'Submit'
+
+};
+
+const pageRegisterSelectors = {
+    h1: 'h1',
+    description: 'p',
+    submitButton: 'form button[type="submit"]',
+    firstNameInput: 'form input[name="firstName"]',
+    lastNameInput: 'form input[name="lastName"]',
+    phoneInput: 'form input[name="phone"]',
+    emailInput: 'form input[name="email"]',
+    passwordInput: 'form input[name="password"]',
+    aboutInput: 'form textarea[name="about"]',
+    goalsInput: 'form textarea[name="goals"]',
+    englishLevelInput: 'form select[name="englishLevel"]'
+};
+
+const pageLogin = {
+    h1: 'You are a new user'
+};
+
+module.exports = {URL_REGISTER, URL_LOGIN, user, page, pageRegisterSelectors, pageLogin};
